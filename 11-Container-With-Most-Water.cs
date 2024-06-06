728a54e34dd9bbbdb795bcbb345d4a84c459f7c8
Yahia Sherif <120673276+Yahiasherif002@users.noreply.github.com>
2024-06-06 23:08:36 +0300

    [11] [Time Beats: 18.37%] [Memory Beats: 97.27%] - LeetPush

---
+++ 11-Container-With-Most-Water.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int MaxArea(int[] height) {
+        int res=0, area=0,left=0,right=height.Length-1;
+        while(left<right){
+             area= (Math.Min(height[left],height[right])*(right-left));
+             res= Math.Max(res,area);
+
+            if(height[left]<height[right]){
+                left++;
+            }else{
+                right--;
+            }
+        }
+        return res;
+    }
+}
