0024174961411c6215efcf092f2a84d165484598
Yahia Sherif <120673276+Yahiasherif002@users.noreply.github.com>
2024-06-22 17:33:49 +0300

    [242] [Time Beats: 63.03%] [Memory Beats: 49.37%] - LeetPush

--- 242-Valid-Anagram.cs
+++ 242-Valid-Anagram.cs
@@ -1,21 +1,25 @@
 public class Solution {
     public bool IsAnagram(string s, string t) {
+        if(s.Length != t.Length) return false;
+        if(s==t) return true;
 
- if(s.Length != t.Length ) return false;
- if(s==t) return true;
+        Dictionary <char,int> sCount= new Dictionary <char,int>();
+        Dictionary <char,int> tCount= new Dictionary <char,int>();
 
-   Dictionary<char,int>  sCounts= new  Dictionary<char, int>();
-    Dictionary<char,int>  tCounts= new  Dictionary<char, int>();
+        for(int i=0;i<s.Length;i++){
+            sCount[s[i]]=1+(sCount.ContainsKey(s[i]) ? sCount[s[i]] :0);
+            tCount[t[i]]=1+(tCount.ContainsKey(t[i]) ? tCount[t[i]] :0);
+        }
 
-       for(int i=0; i< s.Length;i++){
-        sCounts[s[i]]=1+(sCounts.ContainsKey(s[i]) ? sCounts[s[i]]:0 );
-        tCounts[t[i]]=1+(tCounts.ContainsKey(t[i]) ? tCounts[t[i]]:0 );
-       }
+        foreach (char c in sCount.Keys) {
+            if (!tCount.ContainsKey(c) || sCount[c] != tCount[c]) {
+                return false;
+            }
+        }
 
-       foreach(char c in sCounts.Keys){
-        int tCount = (tCounts.ContainsKey(c)?tCounts[c]:0);
-        if(sCounts[c]!=tCount) return false;
-       }
-       return true;
+        return true;
+
+
+ 
     }
 }
