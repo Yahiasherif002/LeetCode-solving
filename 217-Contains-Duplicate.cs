09d270023ebd1f83f295f8b86e1a6bc6e3508217
Yahia Sherif <120673276+Yahiasherif002@users.noreply.github.com>
2024-06-22 17:39:16 +0300

    [217] [Time Beats: 54.44%] [Memory Beats: 11.41%] - LeetPush

--- 217-Contains-Duplicate.cs
+++ 217-Contains-Duplicate.cs
@@ -1,5 +1,11 @@
 public class Solution {
     public bool ContainsDuplicate(int[] nums) {
-        return nums.Length!=nums.Distinct().Count();
+        HashSet<int> set = new HashSet<int>();
+        foreach(var num in nums){
+            if(set.Contains(num)) return true;
+            set.Add(num);
+        }
+        return false;
+
     }
 }
