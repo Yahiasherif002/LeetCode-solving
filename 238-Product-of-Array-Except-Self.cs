2de5d66fb32f2c110db55c52963835a35e09079d
Yahia Sherif <120673276+Yahiasherif002@users.noreply.github.com>
2024-07-08 23:32:05 +0300

    [238] [Time Beats: 60.26%] [Memory Beats: 73.77%] - LeetPush

--- 238-Product-of-Array-Except-Self.cs
+++ 238-Product-of-Array-Except-Self.cs
@@ -2,20 +2,19 @@ public class Solution
 {
     public int[] ProductExceptSelf(int[] nums)
     {
-        int n = nums.Length;
-        int[] output = new int[n];
-        output[0] = 1;
-        for (int i = 1; i < n; i++)
-        {
-            output[i] = output[i - 1] * nums[i - 1]; // output[i] = left product of i element in nums   array 
-        }
-        int right = 1;
-        for (int i = n - 1; i >= 0; i--)
-        {
-            output[i] *= right; // output[i] = left product of i element in nums   array * right product of i element in nums   array
-            right *= nums[i];
-        }
-        return output;
-    }
+        int pre=1;
+        int post=1;
+        int [] result = new int[nums.Length];
 
+        for(int i =0;i<nums.Length;i++){
+            result[i]=pre;
+            pre*=nums[i];
+        }
+        for(int i = nums.Length-1;i>=0;i--){
+            result[i]*=post;
+            post*=nums[i];
+        }
+
+        return result;
+    }
 }
