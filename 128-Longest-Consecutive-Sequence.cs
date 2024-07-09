6bdaa355d3662f6fc4f2fde706977b75595a2c0f
Yahia Sherif <120673276+Yahiasherif002@users.noreply.github.com>
2024-07-09 22:20:17 +0300

    [128] [Time Beats: 5.02%] [Memory Beats: 64.08%] - LeetPush

--- 128-Longest-Consecutive-Sequence.cs
+++ 128-Longest-Consecutive-Sequence.cs
@@ -1,15 +1,15 @@
 public class Solution {
     public int LongestConsecutive(int[] nums) {
-        var numSet= new HashSet<int>(nums);
-        int longest =0;
-        foreach(var n in nums){
-            if(!numSet.Contains(n-1)){
-                int length =0;
-                while(numSet.Contains(n+length++)){
-                    longest= Math.Max(longest,length);
-                }
-            }
+       var numSet= new HashSet<int>(nums);
+       int longest=0;
+       foreach(var num in nums){
+        if(!numSet.Contains(num-1)){
+             int length=0;
+             while(numSet.Contains(num+length++)){
+                longest=Math.Max(length,longest);
+             }
         }
-        return longest;
+       }
+       return longest;
     }
 }
