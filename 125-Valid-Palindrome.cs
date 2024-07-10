26e1f6ac46bb91113fb23cae45811e1ecc4a3ea6
Yahia Sherif <120673276+Yahiasherif002@users.noreply.github.com>
2024-07-10 23:46:12 +0300

    [125] [Time Beats: 50.29%] [Memory Beats: 59.94%] - LeetPush

---
+++ 125-Valid-Palindrome.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public bool IsPalindrome(string s) {
+        if(s.Trim()==\ \) return true;
+        int left=0;
+        int right= s.Length-1;
+
+        while(left<right){
+            if(!char.IsLetterOrDigit(s[left])) left++;
+            else if(!char.IsLetterOrDigit(s[right])) right--;
+            else if (char.ToLower(s[left++])!=char.ToLower(s[right--])) return false;
+
+
+        }
+
+
+        return true;
+    }
+}
